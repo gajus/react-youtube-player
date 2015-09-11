0210ccfef9f0cd9d7710ccf2275505df10e03350
Gajus Kuizinas <gk@anuary.com>
2015-09-11 11:23:44 +0100

    First commit.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * @property {String} videoId
+ * @property {String} state ('playing', 'stopped', 'paused')
+ */
+class YouTubePlayer extends React.Component {
+    render () {
+
+    }
+}
