987606674c763967834aa430e4db15fb00d61d6f
Gajus Kuizinas <gajus@gajus.com>
2016-05-10 21:10:22 +0100

    Fix code style.

--- src/stories/player.js
+++ src/stories/player.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import {
-    storiesOf,
-    action
+    storiesOf
 } from '@kadira/storybook';
 import YouTubePlayer from './../';
 
 storiesOf('react-youtube-player', module)
-    .add('component mounted with playbackState set to "unstarted"', () => (
-        <YouTubePlayer
-            videoId='M7lc1UVf-VE'
-            width='640'
+    .add('component mounted with playbackState set to "unstarted"', () => {
+        return <YouTubePlayer
             height='360'
             playbackState='unstarted'
-        />
-    ))
-    .add('component mounted with playbackState set to "playing"', () => (
-        <YouTubePlayer
             videoId='M7lc1UVf-VE'
             width='640'
+        />;
+    })
+    .add('component mounted with playbackState set to "playing"', () => {
+        return <YouTubePlayer
             height='360'
             playbackState='playing'
-        />
-    ))
-    .add('component mounted with playbackState set to "paused"', () => (
-        <YouTubePlayer
             videoId='M7lc1UVf-VE'
             width='640'
+        />;
+    })
+    .add('component mounted with playbackState set to "paused"', () => {
+        return <YouTubePlayer
             height='360'
             playbackState='paused'
-        />
-    ));
+            videoId='M7lc1UVf-VE'
+            width='640'
+        />;
+    });
